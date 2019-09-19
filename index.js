332ae48bcce00ad32c769d993f4190ea8a9388cc
vail <ysywix@gmail.com>
2019-09-19 19:13:13 +0300

    Correct work when status is pending

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//TODO make safe constructor
 function Promise (func) {
   this.state = 'pending'
   this.value = null
   this.reason = null
+  this.resolveCallbacks = []
+  this.rejectCallbacks = []
 
   let fulfill = this.fulfill.bind(this)
   let reject = this.reject.bind(this)
@@ -15,50 +18,71 @@ function Promise (func) {
 Promise.prototype.fulfill = function (value) {
   this.state = 'fulfilled'
   this.value = value
+  this.resolveCallbacks.forEach(function (callback) {
+    callback()
+  })
+  delete this.resolveCallbacks
 }
 
 //TODO make it private
 Promise.prototype.reject = function (reason) {
   this.state = 'rejected'
   this.reason = reason
+  this.rejectCallbacks.forEach(function (callback) {
+    callback()
+  })
+  delete this.rejectCallbacks
 }
 
 Promise.prototype.then = function (onFulfilled, onRejected) {
   const self = this
 
-  if (this.state === 'fulfilled') {
+  //TODO Make it global and bind it to Promise
+  const onFulfilledWrapper = function (resolve, reject) {
     if (typeof onFulfilled === 'function') {
-      return new Promise(function (resolve, reject) {
-        try {
-          const value = onFulfilled(self.value)
-          resolve(value)
-        } catch (e) {
-          reject(e)
-        }
-      })
+      try {
+        const value = onFulfilled(self.value)
+        //TODO work when value is Promise
+        resolve(value)
+      } catch (e) {
+        reject(e)
+      }
     } else {
-      return Promise.resolve(this.value)
+      resolve(self.value) // Go to next handler
     }
   }
 
-  if (this.state === 'rejected') {
+  //TODO Make it global and bind it to Promise
+  const onRejectedWrapper = function (resolve, reject) {
     if (typeof onRejected === 'function') {
-      return new Promise(function (resolve, reject) {
-        try {
-          const value = onRejected(self.reason)
-          resolve(value)
-        } catch (e) {
-          reject(e)
-        }
-      })
+      try {
+        const value = onRejected(self.reason)
+        //TODO work when value is Promise
+        resolve(value)
+      } catch (e) {
+        reject(e)
+      }
     } else {
-      return Promise.reject(self.reason)
+      reject(self.reason) // Go to next handler
     }
   }
 
-  //TODO implement work with pending status
   return new Promise(function (resolve, reject) {
-
+    switch (self.state) {
+      case 'fulfilled':
+        onFulfilledWrapper(resolve, reject)
+        break
+      case 'rejected':
+        onRejectedWrapper(resolve, reject)
+        break
+      case 'pending':
+        self.resolveCallbacks.push(function () {
+          onFulfilledWrapper(resolve, reject)
+        })
+        self.rejectCallbacks.push(function () {
+          onRejectedWrapper(resolve, reject)
+        })
+    }
   })
 }
 
